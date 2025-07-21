59999ebd00795f84ebedbfec2874d39d14d06e87
pgarrigos <pgarrigos@renr.es>
2025-07-21 16:08:21 +0200

    Realizando ejercicios Ts

---
+++ ejercicio5.js
@@ -0,0 +1,8 @@
+"use strict";
+function sumar(numero1, numero2) {
+    return numero1 + numero2;
+}
+const a = 8;
+const b = 20;
+const resultado = sumar(a, b);
+console.log("La suma es:" + resultado);
---
+++ ejercicio5.ts
@@ -0,0 +1,8 @@
+function sumar(numero1: number, numero2: number): number {
+  return numero1 + numero2;
+}
+
+const a: number = 8;
+const b: number = 20;
+const resultado: number = sumar(a, b);
+console.log("La suma es:" + resultado);
